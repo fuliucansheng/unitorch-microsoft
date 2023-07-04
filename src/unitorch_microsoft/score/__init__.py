4032592dbe285f4b60a01eed73411ce563ba090a
Desheng Cui <decu@microsoft.com>
2023-07-04 10:57:20 +0000

    Merged PR 24: Add SLAB Bert Ranking V2.2
    Add SLAB Bert Ranking V2.2

--- src/unitorch_microsoft/score/__init__.py
+++ src/unitorch_microsoft/score/__init__.py
@@ -7,31 +7,3 @@ from unitorch.cli.models import ClassificationOutputs, ClassificationTargets
 from unitorch.cli.score import Score
 
 
-@register_score("microsoft/score/pearsonr_corr")
-class PearsonrCorrScore(Score):
-    def __init__(
-        self,
-    ):
-        super().__init__()
-
-    @classmethod
-    @add_default_section_for_init("microsoft/score/pearsonr_corr")
-    def from_core_configure(cls, config, **kwargs):
-        pass
-
-    def forward(
-        self,
-        outputs: ClassificationOutputs,
-        targets: ClassificationTargets,
-    ):
-        if isinstance(outputs, ClassificationOutputs):
-            outputs = outputs.outputs
-        if isinstance(targets, ClassificationTargets):
-            targets = targets.targets
-
-        outputs = outputs.view(-1)
-        targets = targets.view(-1)
-
-        assert outputs.numel() == targets.numel()
-
-        return np.corrcoef(targets, outputs).sum() / 2 - 1
