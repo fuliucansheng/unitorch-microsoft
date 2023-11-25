6d20c7aefae9314b0c19cf06a3983584c48e1714
Desheng Cui <decu@microsoft.com>
2023-11-25 12:34:46 +0000

    Merged PR 120: bert argus v2 for irp relevance
    bert argus v2 for irp relevance

--- src/unitorch_microsoft/vpr/bert.py
+++ src/unitorch_microsoft/vpr/bert.py
@@ -178,7 +178,7 @@ class BertForPretrainV2(GenericModel):
         self.init_weights()
 
     @classmethod
-    @add_default_section_for_init("microsoft/vpr/pretrain/bert")
+    @add_default_section_for_init("microsoft/vpr/pretrain/bert/v2")
     def from_core_configure(cls, config, **kwargs):
         config.set_default_section("microsoft/vpr/pretrain/bert/v2")
         pretrained_name = config.getoption("pretrained_name", "default-bert")
@@ -356,6 +356,115 @@ class BertForClassification(GenericModel):
 
         query_embeds = query_embeds / query_embeds.norm(dim=-1, keepdim=True)
         # image_embeds = image_embeds / image_embeds.norm(dim=-1, keepdim=True)
+
+        scores = torch.sum(query_embeds * image_embeds, dim=-1, keepdim=True)
+        outputs = self.classifier(scores)
+
+        return ClassificationOutputs(outputs=outputs)
+
+@register_model("microsoft/vpr/classification/bert/v2")
+class BertForClassificationV2(GenericModel):
+    replace_keys_in_state_dict = {"gamma": "weight", "beta": "bias"}
+
+    def __init__(
+        self,
+        config_path: str,
+        num_hidden_layers: Optional[int] = 12,
+        image_embed_dim: Optional[int] = 100,
+        gradient_checkpointing: Optional[bool] = False,
+        output_query_embed: Optional[bool] = False,
+        output_image_embed: Optional[bool] = False,
+    ):
+        super().__init__()
+        self.config = BertConfig.from_json_file(config_path)
+        self.config.num_hidden_layers = num_hidden_layers
+        self.config.gradient_checkpointing = gradient_checkpointing
+        self.image_embed_dim = image_embed_dim
+        self.bert = BertModel(self.config, add_pooling_layer=False)
+        self.query_projection = nn.Linear(self.config.hidden_size, image_embed_dim)
+        self.image_projection = nn.Linear(
+            self.image_embed_dim,
+            self.image_embed_dim,
+        )
+
+        self.output_query_embed = output_query_embed
+        self.output_image_embed = output_image_embed
+        self.classifier = nn.Linear(1, 1)
+
+        self.init_weights()
+        self.classifier.weight.data.fill_(5.0)
+
+    @classmethod
+    @add_default_section_for_init("microsoft/vpr/classification/bert/v2")
+    def from_core_configure(cls, config, **kwargs):
+        config.set_default_section("microsoft/vpr/classification/bert/v2")
+        pretrained_name = config.getoption("pretrained_name", "default-bert")
+        config_path = config.getoption("config_path", None)
+        num_hidden_layers = config.getoption("num_hidden_layers", 12)
+        output_query_embed = config.getoption("output_query_embed", False)
+        output_image_embed = config.getoption("output_image_embed", False)
+        config_path = pop_value(
+            config_path,
+            nested_dict_value(pretrained_bert_infos, pretrained_name, "config"),
+        )
+        config_path = cached_path(config_path)
+        gradient_checkpointing = config.getoption("gradient_checkpointing", False)
+        inst = cls(
+            config_path=config_path,
+            num_hidden_layers=num_hidden_layers,
+            gradient_checkpointing=gradient_checkpointing,
+            output_query_embed=output_query_embed,
+            output_image_embed=output_image_embed,
+        )
+        pretrained_weight_path = config.getoption("pretrained_weight_path", None)
+        weight_path = pop_value(
+            pretrained_weight_path,
+            nested_dict_value(pretrained_bert_infos, pretrained_name, "weight"),
+            check_none=False,
+        )
+        if weight_path is not None:
+            weight_path = cached_path(weight_path)
+            inst.from_pretrained(weight_path)
+
+        return inst
+
+    @autocast()
+    def forward(
+        self,
+        input_ids: Optional[torch.Tensor] = None,
+        image_embeds: Optional[torch.Tensor] = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        token_type_ids: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.Tensor] = None,
+    ):
+        if self.output_query_embed:
+            query_outputs = self.bert(
+                input_ids,
+                attention_mask=attention_mask,
+                token_type_ids=token_type_ids,
+                position_ids=position_ids,
+            )
+            query_embeds = self.query_projection(query_outputs[0][:, 0])
+
+            query_embeds = query_embeds / query_embeds.norm(dim=-1, keepdim=True)
+            return EmbeddingOutputs(embedding=query_embeds)
+
+        if self.output_image_embed:
+            image_embeds = self.image_projection(image_embeds)
+            image_embeds = image_embeds / image_embeds.norm(dim=-1, keepdim=True)
+            return EmbeddingOutputs(embedding=image_embeds)
+
+        query_outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+        )
+        query_embeds = self.query_projection(query_outputs[0][:, 0])
+        image_embeds = self.image_projection(image_embeds)
+
+        query_embeds = query_embeds / query_embeds.norm(dim=-1, keepdim=True)
+        image_embeds = image_embeds / image_embeds.norm(dim=-1, keepdim=True)
 
         scores = torch.sum(query_embeds * image_embeds, dim=-1, keepdim=True)
         outputs = self.classifier(scores)
