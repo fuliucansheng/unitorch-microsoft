f81fbc433906e515e9bbbf797d299cc95c80a852
Qiangqiang Zhu <qianzhu@microsoft.com>
2023-11-28 05:13:01 +0000

    Merged PR 122: add new feature variants for experiment
    add new feature variants for experiment

--- src/unitorch_microsoft/msan/l1/__init__.py
+++ src/unitorch_microsoft/msan/l1/__init__.py
@@ -1,6 +1,6 @@
 # Copyright (c) MICROSOFT.
 # Licensed under the MIT License.
-
+import unitorch_microsoft.msan.l1.modeling_bletchley_v1_exp1
 import unitorch_microsoft.msan.l1.modeling_bletchley_v1
 import unitorch_microsoft.msan.l1.processing_bletchley_v1
 import unitorch_microsoft.msan.l1.modeling_tulr_v6
