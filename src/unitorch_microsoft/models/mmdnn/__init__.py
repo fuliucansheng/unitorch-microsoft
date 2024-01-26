37dc0985688f666919bfb230cf572dbb7061f343
Lixin Qian <lixinqian@microsoft.com>
2024-01-26 06:27:51 +0000

    Merged PR 173: Add PA INTL Model With ResLayer

--- src/unitorch_microsoft/models/mmdnn/__init__.py
+++ src/unitorch_microsoft/models/mmdnn/__init__.py
@@ -5,3 +5,4 @@ import unitorch_microsoft.models.mmdnn.modeling_bletchley_v1
 import unitorch_microsoft.models.mmdnn.modeling_bletchley_v3
 import unitorch_microsoft.models.mmdnn.modeling_bletchley_v3_intl
 import unitorch_microsoft.models.mmdnn.modeling_bletchley_v3_reslayer
+import unitorch_microsoft.models.mmdnn.modeling_bletchley_v3_intl_reslayer
