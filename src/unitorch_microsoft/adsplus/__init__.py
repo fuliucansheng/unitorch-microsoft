0fa79239f7a75fed8fcf2402e4741b0c00a82f88
Yun Liu <yunliu2@microsoft.com>
2024-02-01 11:04:34 +0000

    Merged PR 178: Add bletchley v1 model and config for PA&IRP click ranking
    Add bletchley v1 model and config  for PA&IRP click ranking

--- src/unitorch_microsoft/adsplus/__init__.py
+++ src/unitorch_microsoft/adsplus/__init__.py
@@ -4,5 +4,6 @@
 import unitorch_microsoft.adsplus.click.autonlp
 import unitorch_microsoft.adsplus.click.modeling_bert
 import unitorch_microsoft.adsplus.image.bletchley_v1
+import unitorch_microsoft.adsplus.image.bletchley_v1_click
 import unitorch_microsoft.adsplus.relevance
 import unitorch_microsoft.adsplus.slab.modeling_bert
