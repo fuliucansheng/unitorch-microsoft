26eedd972853dbe07b762de5ec42aa8bc9373367
Chang Xu <xucha@microsoft.com>
2024-04-15 07:03:51 +0000

    Merged PR 219: support match function
    support match function

--- src/unitorch_microsoft/china/selection/finetune/modeling_v1.py
+++ src/unitorch_microsoft/china/selection/finetune/modeling_v1.py
@@ -257,3 +257,145 @@ class BletchleyForTextRetrieval(GenericModel):
         loss = loss / ((samples.shape[1] - 1) * samples.shape[0])
 
         return LossOutputs(loss=loss)
+
+@register_model("microsoft/china/selection/finetune/matching/bletchley/v1")
+class BletchleyForMatching(GenericModel):
+    replace_keys_in_state_dict = {
+        "text_encoder.projection": "text_projection",
+        "image_encoder.projection": "image_projection",
+    }
+
+    def __init__(
+        self,
+        query_config_type: str,
+	doc_config_type: str,
+        projection_dim: Optional[int] = 1024,
+        freeze_base_model: Optional[bool] = True,
+        gradient_checkpointing: Optional[bool] = False,
+        output_query_embed: Optional[bool] = False,
+        output_doc_embed: Optional[bool] = False,
+    ):
+        super().__init__()
+        query_config = get_bletchley_text_config(query_config_type, gradient_checkpointing)
+        doc_config = get_bletchley_text_config(doc_config_type, gradient_checkpointing)
+
+        self.output_query_embed = output_query_embed
+        self.output_doc_embed = output_doc_embed
+
+        self.query_embed_dim = query_config.hidden_size
+        self.doc_embed_dim = doc_config.hidden_size
+
+        self.query_encoder = BletchleyTextEncoder(
+            query_config, add_projection_layer=False
+        )
+        self.doc_encoder = BletchleyTextEncoder(
+            doc_config, add_projection_layer=False
+        )
+
+        self.query_projection = nn.Linear(
+            self.query_embed_dim,
+            projection_dim,
+        )  # text_encoder.projection.weight, text_encoder.projection.bias
+        self.doc_projection = nn.Linear(
+            self.doc_embed_dim,
+            projection_dim,
+        )  # image_encoder.projection.weight,  image_encoder.projection.bias
+
+        self.classifier = nn.Linear(1, 1)
+
+        self.init_weights()
+        self.classifier.weight.data.fill_(5.0)
+
+        if freeze_base_model:
+            for p in self.query_encoder.parameters():
+                p.requires_grad = False
+
+            for p in self.doc_encoder.parameters():
+                p.requires_grad = False
+
+    @classmethod
+    @add_default_section_for_init("microsoft/china/selection/finetune/matching/bletchley/v1")
+    def from_core_configure(cls, config, **kwargs):
+        config.set_default_section("microsoft/china/selection/finetune/matching/bletchley/v1")
+        query_config_type = config.getoption("query_config_type", "0.8B")
+        doc_config_type = config.getoption("doc_config_type", "0.8B")
+
+        projection_dim = config.getoption("projection_dim", 1024)
+        freeze_base_model = config.getoption("freeze_base_model", True)
+        gradient_checkpointing = config.getoption("gradient_checkpointing", False)
+        output_query_embed = config.getoption("output_query_embed", False)
+        output_doc_embed = config.getoption("output_doc_embed", False)
+
+        inst = cls(
+            query_config_type=query_config_type,
+	    doc_config_type=doc_config_type,
+            projection_dim=projection_dim,
+            freeze_base_model=freeze_base_model,
+            gradient_checkpointing=gradient_checkpointing,
+            output_query_embed=output_query_embed,
+            output_doc_embed=output_doc_embed,
+        )
+        pretrained_weight_path = config.getoption("pretrained_weight_path", None)
+        if pretrained_weight_path is not None:
+            inst.from_pretrained(pretrained_weight_path)
+
+        return inst
+
+    def from_pretrained(self, weight_path):
+        if not os.path.exists(weight_path):
+            return
+        state_dict = torch.load(weight_path, map_location="cpu")
+        _keys = [key for key in state_dict.keys() if key.startswith("text_encoder")]
+        for _key in _keys:
+            _value = state_dict.pop(_key)
+            state_dict["query_encoder" + _key[12:]] = _value
+            state_dict["doc_encoder" + _key[12:]] = _value
+
+        super().from_pretrained(state_dict=state_dict)
+
+    @autocast()
+    def forward(
+        self,
+        query_input_ids=None,
+        query_attention_mask=None,
+        doc_input_ids=None,
+        doc_attention_mask=None,
+    ):
+        if not self.training and self.output_query_embed:
+            query_outputs = self.query_encoder(
+                input_ids=query_input_ids, attention_mask=query_attention_mask
+            )
+            query_embeds = query_outputs[:, 0]
+            query_embeds = self.query_projection(query_embeds)
+            query_embeds = query_embeds / query_embeds.norm(dim=-1, keepdim=True)
+            return EmbeddingOutputs(embedding=query_embeds)
+
+        if not self.training and self.output_doc_embed:
+            doc_outputs = self.doc_encoder(
+                input_ids=doc_input_ids,
+                attention_mask=doc_attention_mask,
+            )
+            doc_embeds = doc_outputs[:, 0]
+            doc_embeds = self.doc_projection(doc_embeds)
+            doc_embeds = doc_embeds / doc_embeds.norm(dim=-1, keepdim=True)
+            return EmbeddingOutputs(embedding=doc_embeds)
+        query_outputs = self.query_encoder(
+            input_ids=query_input_ids, attention_mask=query_attention_mask
+        )
+        doc_outputs = self.doc_encoder(
+            input_ids=doc_input_ids, attention_mask=doc_attention_mask
+        )
+
+        query_embeds = query_outputs[:, 0]
+        query_embeds = self.query_projection(query_embeds)
+
+        doc_embeds = doc_outputs[:, 0]
+        doc_embeds = self.doc_projection(doc_embeds)
+
+
+        query_embeds = query_embeds / query_embeds.norm(dim=-1, keepdim=True)
+        doc_embeds = doc_embeds / doc_embeds.norm(dim=-1, keepdim=True)
+        scores = torch.sum(query_embeds * doc_embeds, dim=-1, keepdim=True)
+
+        outputs = self.classifier(scores)
+        return ClassificationOutputs(outputs=outputs)
