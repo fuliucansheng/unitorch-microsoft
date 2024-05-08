859d4deb7f73a3123a932ef50ee84021342a382e
Chang Xu <xucha@microsoft.com>
2024-05-08 07:50:57 +0000

    Merged PR 241: support quantize for pretrain model
    support quantize for pretrain model

--- src/unitorch_microsoft/china/selection/modeling_v1.py
+++ src/unitorch_microsoft/china/selection/modeling_v1.py
@@ -38,6 +38,173 @@ from unitorch_microsoft.models.bletchley.modeling_v1 import (
 )
 
 
+@register_model("microsoft/china/selection/pretrain/v1/text")
+class BletchleyForTextPretrain(GenericModel):
+    def __init__(
+        self,
+        query_config_type: str,
+        doc_config_type: str,
+        projection_dim: Optional[int] = 1024,
+        freeze_base_model: Optional[bool] = True,
+        logit_scale_init_value: Optional[float] = 2.6592,
+        gradient_checkpointing: Optional[bool] = False,
+        use_all_gather: Optional[bool] = True,
+        output_query_embed: Optional[bool] = False,
+        output_doc_embed: Optional[bool] = False,
+        enable_quantization: Optional[bool] = False,
+    ):
+        super().__init__()
+        query_config = get_bletchley_text_config(
+            query_config_type, gradient_checkpointing
+        )
+        doc_config = get_bletchley_text_config(doc_config_type, gradient_checkpointing)
+
+        self.use_all_gather = use_all_gather
+        self.output_query_embed = output_query_embed
+        self.output_doc_embed = output_doc_embed
+
+        self.query_embed_dim = query_config.hidden_size
+        self.doc_embed_dim = doc_config.hidden_size
+
+        self.query_encoder = BletchleyTextEncoder(
+            query_config, add_projection_layer=False
+        )
+        self.doc_encoder = BletchleyTextEncoder(doc_config, add_projection_layer=False)
+
+        self.query_projection = nn.Linear(
+            self.query_embed_dim,
+            projection_dim,
+        )
+        self.doc_projection = nn.Linear(
+            self.doc_embed_dim,
+            projection_dim,
+        )
+
+        self.logit_scale = nn.Parameter(torch.ones([]) * logit_scale_init_value)
+        self.init_weights()
+
+        if enable_quantization:
+            for __model__ in [
+                self.query_encoder,
+                self.query_projection
+            ]:
+                __model__.qconfig = torch.quantization.get_default_qat_qconfig(
+                    version=0
+                )
+                torch.quantization.prepare_qat(__model__, inplace=True)            
+
+        if freeze_base_model:
+            for p in self.query_encoder.parameters():
+                p.requires_grad = False
+
+            for p in self.doc_encoder.parameters():
+                p.requires_grad = False
+
+    @classmethod
+    @add_default_section_for_init("microsoft/china/selection/pretrain/v1/text")
+    def from_core_configure(cls, config, **kwargs):
+        config.set_default_section("microsoft/china/selection/pretrain/v1/text")
+        query_config_type = config.getoption("query_config_type", "0.8B")
+        doc_config_type = config.getoption("doc_config_type", "0.8B")
+
+        projection_dim = config.getoption("projection_dim", 1024)
+        freeze_base_model = config.getoption("freeze_base_model", True)
+        logit_scale_init_value = config.getoption("logit_scale_init_value", 2.6592)
+        gradient_checkpointing = config.getoption("gradient_checkpointing", False)
+        use_all_gather = config.getoption("use_all_gather", True)
+        output_query_embed = config.getoption("output_query_embed", False)
+        output_doc_embed = config.getoption("output_doc_embed", False)
+        enable_quantization = config.getoption("enable_quantization", False)
+
+        inst = cls(
+            query_config_type=query_config_type,
+            doc_config_type=doc_config_type,
+            projection_dim=projection_dim,
+            freeze_base_model=freeze_base_model,
+            logit_scale_init_value=logit_scale_init_value,
+            gradient_checkpointing=gradient_checkpointing,
+            use_all_gather=use_all_gather,
+            output_query_embed=output_query_embed,
+            output_doc_embed=output_doc_embed,
+            enable_quantization=enable_quantization,
+        )
+        pretrained_weight_path = config.getoption("pretrained_weight_path", None)
+        if pretrained_weight_path is not None:
+            pretrained_weight_path = cached_path(pretrained_weight_path)
+            inst.from_pretrained(pretrained_weight_path)
+
+        return inst
+
+    def from_pretrained(self, weight_path):
+        if not os.path.exists(weight_path):
+            return
+        state_dict = torch.load(weight_path, map_location="cpu")
+        _keys = [key for key in state_dict.keys() if key.startswith("text_encoder")]
+        for _key in _keys:
+            _value = state_dict.pop(_key)
+            state_dict["query_encoder" + _key[12:]] = _value
+            state_dict["doc_encoder" + _key[12:]] = _value
+
+        super().from_pretrained(state_dict=state_dict)
+
+    def all_gather(self, input):
+        output = AllGather.apply(input)
+        output = output.view(-1, *(output.shape[2:]))
+        return output
+
+    @autocast()
+    def forward(
+        self,
+        query_input_ids=None,
+        query_attention_mask=None,
+        doc_input_ids=None,
+        doc_attention_mask=None,
+    ):
+        if not self.training and self.output_query_embed:
+            query_outputs = self.query_encoder(
+                input_ids=query_input_ids, attention_mask=query_attention_mask
+            )
+            query_embeds = query_outputs[:, 0]
+            query_embeds = self.query_projection(query_embeds)
+            query_embeds = query_embeds / query_embeds.norm(dim=-1, keepdim=True)
+
+            return EmbeddingOutputs(embedding=query_embeds)
+
+        if not self.training and self.output_doc_embed:
+            doc_outputs = self.doc_encoder(
+                input_ids=doc_input_ids, attention_mask=doc_attention_mask
+            )
+            doc_embeds = doc_outputs[:, 0]
+            doc_embeds = self.doc_projection(doc_embeds)
+            doc_embeds = doc_embeds / doc_embeds.norm(dim=-1, keepdim=True)
+
+            return EmbeddingOutputs(embedding=doc_embeds)
+        query_outputs = self.query_encoder(
+            input_ids=query_input_ids, attention_mask=query_attention_mask
+        )
+        doc_outputs = self.doc_encoder(
+            input_ids=doc_input_ids, attention_mask=doc_attention_mask
+        )
+
+        query_embeds = query_outputs[:, 0]
+        query_embeds = self.query_projection(query_embeds)
+
+        doc_embeds = doc_outputs[:, 0]
+        doc_embeds = self.doc_projection(doc_embeds)
+
+        query_embeds = query_embeds / query_embeds.norm(dim=-1, keepdim=True)
+        doc_embeds = doc_embeds / doc_embeds.norm(dim=-1, keepdim=True)
+
+        logit_scale = self.logit_scale.exp()
+        if self.use_all_gather and dist.is_initialized():
+            query_embeds = self.all_gather(query_embeds)
+            doc_embeds = self.all_gather(doc_embeds)
+        logits_per_text = torch.matmul(query_embeds, doc_embeds.t()) * logit_scale
+
+        loss = _clip_loss(logits_per_text)
+
+        return LossOutputs(loss=loss)
+
 @register_model("microsoft/china/selection/retrieval/v1/text")
 class BletchleyForTextRetrieval(GenericModel):
     def __init__(
@@ -414,3 +581,4 @@ class BletchleyForMatching(GenericModel):
 
         outputs = self.classifier(scores)
         return ClassificationOutputs(outputs=outputs)
+
