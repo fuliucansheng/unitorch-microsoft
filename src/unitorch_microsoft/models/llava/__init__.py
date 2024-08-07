5611039988126a30f6ac4b2a62ea081b32cfd7f3
Jinjin Chen <jinjinchen@microsoft.com>
2024-08-07 08:00:56 +0000

    Merged PR 304: Add MMAExtensionFinetunedBletchleyCKPTForLLAVA
    Add MMAExtensionFinetunedBletchleyCKPTForLLAVA

--- src/unitorch_microsoft/models/llava/__init__.py
+++ src/unitorch_microsoft/models/llava/__init__.py
@@ -31,6 +31,17 @@ pretrained_llava_infos = {
                 for i in range(1, 5)
             ],
         },
+        "llava-v1.6-mistral-7b-MMAExtension-bletchley-v3-2.5b": {
+            "config": "https://huggingface.co/llava-hf/llava-v1.6-mistral-7b-hf/resolve/main/config.json",
+            "vocab": "https://huggingface.co/llava-hf/llava-v1.6-mistral-7b-hf/resolve/main/tokenizer.model",
+            "weight": [
+                "https://unitorchazureblob.blob.core.windows.net/shares/models/bletchley/v3/MMAExtensionFinetune/pytorch_model.bin"
+            ]
+            + [
+                f"https://huggingface.co/llava-hf/llava-v1.6-mistral-7b-hf/resolve/main/model-{str(i).rjust(5, '0')}-of-00004.safetensors"
+                for i in range(1, 5)
+            ],
+        },
     },
 }
 
