e1952cae2449ff6fd84a13693fee1f708d82b88e
Desheng Cui <Desheng.Cui@microsoft.com>
2025-01-28 14:39:46 +0000

    Merged PR 374: Updated wikiart.py
    Updated wikiart.py

--- src/unitorch_microsoft/picasso/crawlers/wikiart.py
+++ src/unitorch_microsoft/picasso/crawlers/wikiart.py
@@ -64,11 +64,11 @@ def main(
     def save_to_zip(image):
         md5 = hashlib.md5()
         md5.update(image.tobytes())
-        name = md5.hexdigest() + ".png"
+        name = md5.hexdigest() + ".jpg"
         if name in saved_images:
             return name
         saved_buffer = io.BytesIO()
-        image.save(saved_buffer, format="PNG")
+        image.save(saved_buffer, format="JPG")
         saved_buffer = saved_buffer.getvalue()
         files = {"file": saved_buffer}
         requests.post(f"http://0.0.0.0:11231/?name={name}", files=files)
