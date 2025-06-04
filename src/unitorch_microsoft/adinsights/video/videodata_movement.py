d846cef595dae02775ed4461cf281462db3bc8d9
Chang Xu <xucha@microsoft.com>
2025-06-04 05:54:52 +0000

    Merged PR 519: Datamovement: support retry
    Datamovement: support retry

--- src/unitorch_microsoft/adinsights/video/videodata_movement.py
+++ src/unitorch_microsoft/adinsights/video/videodata_movement.py
@@ -14,6 +14,8 @@ def process_chunk(
     cache_dir,
     num_processes,
     total_rows,
+    file_writer,
+    lock
 ):
 
     if process_id == num_processes - 1:
@@ -23,6 +25,7 @@ def process_chunk(
         f"Worker {process_id} Processing rows: {chunk_start} to {chunk_start + chunk_size - 1} \n"
     )
 
+    movement_str = ""
     for video in chunks:
         try:
             src_file = os.path.join('/datablob/shutterstock', video)
@@ -32,12 +35,18 @@ def process_chunk(
                 cmd = f"cp {src_file} {dst_file}"
                 os.system(cmd)
                 print(f"Worker {process_id} copy {src_file} to {dst_file}")
+                movement_str += f"{video}\t{dst_file}\n"
             else:
                 print(f"Worker {process_id} file {src_file} not found")
                 continue
         except Exception as e:
             print(f"Worker {process_id} error processing {video}: {e}")
             continue
+    
+    if movement_str != "":
+        with lock:
+            file_writer.write(movement_str)
+            file_writer.flush()
 
 
 
@@ -48,6 +57,7 @@ def movement(
     names: Union[str, List[str]] = "video",
     move_col: str = "video",
     max_cnt: int = 100000000,
+    sub_folder: str = "video",
 ):
     """
     Movement of video data, used for video data movement.
@@ -67,13 +77,32 @@ def movement(
         header=None,
         nrows=max_cnt,
     )
+    output_file = f"{dst_dir}/output.tsv"
+    if os.path.exists(output_file):
+        uniques = []
+        with open(output_file, "r") as f:
+            for line in f.readlines():
+                videoid, dst_name = line.strip().split("\t")
+                uniques.append(videoid)
+        data = data[
+            ~data.apply(
+                lambda x: x[move_col] in uniques,
+                axis=1,
+            )
+        ]
+    print(f"Data loaded, total rows to move: {len(data)}")
+    writer = open(output_file, "a+")
     videos = data[move_col].tolist()
     num_processes = mp.cpu_count()
     total_rows = len(videos)
     chunk_size = total_rows // num_processes
 
+    dst_dir = os.path.join(dst_dir, sub_folder)
+
     if not os.path.exists(dst_dir):
         os.makedirs(dst_dir, exist_ok=True)
+    
+    lock = mp.Lock()
 
     print(f"need to process {total_rows} videos")
     with mp.Pool(num_processes) as pool:
@@ -86,6 +115,8 @@ def movement(
                 dst_dir,
                 num_processes,
                 total_rows,
+                writer,
+                lock,
             )
             for i in range(num_processes)
         ]
