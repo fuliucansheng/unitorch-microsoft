483f0a13f9966c12d0c1a2efca10afee0a8fe259
Chang Xu <xucha@microsoft.com>
2025-06-16 09:27:06 +0000

    Merged PR 540: tools: Check fileexists

--- src/unitorch_microsoft/adinsights/video/videodata_movement.py
+++ src/unitorch_microsoft/adinsights/video/videodata_movement.py
@@ -135,5 +135,123 @@ def movement(
     return
 
 
+def process_check(
+    videos,
+    chunk_start,
+    chunk_size,
+    process_id,
+    cache_dir,
+    num_processes,
+    total_rows,
+    file_writer,
+    lock,
+):
+    if process_id == num_processes - 1:
+        chunk_size = total_rows - chunk_start + 1
+    chunks = videos[chunk_start : chunk_start + chunk_size]
+    print(
+        f"Worker {process_id} Processing rows: {chunk_start} to {chunk_start + chunk_size - 1} \n"
+    )
+
+    res_str = ""
+    exist_cnt = 0
+    for idx, video in enumerate(chunks):
+        try:
+            if os.path.exists(video):
+                res_str += f"{video}\n"
+                exist_cnt += 1
+                if idx % 100 == 0:
+                    with lock:
+                        writer = open(file_writer, "a+")
+                        writer.write(res_str)
+                        writer.flush()
+                        writer.close()
+                    res_str = ""
+            else:
+                continue
+        except Exception as e:
+            continue
+
+    if res_str != "":
+        with lock:
+            writer = open(file_writer, "a+")
+            writer.write(res_str)
+            writer.flush()
+            writer.close()
+    print(f"Worker {process_id} processed {len(chunks)} videos, found {exist_cnt} existing videos.")
+
+def checkexists(
+    data_file: str,
+    dst_dir: str,
+    names: Union[str, List[str]] = "video",
+    move_col: str = "video",
+    max_cnt: int = 100000000,
+):
+    """
+    Movement of video data, used for video data movement.
+    """
+
+    if isinstance(names, str) and names.strip() == "*":
+        names = None
+    if isinstance(names, str):
+        names = re.split(r"[,;]", names)
+        names = [n.strip() for n in names]
+    data = pd.read_csv(
+        data_file,
+        names=names,
+        sep="\t",
+        quoting=3,
+        header=None,
+        nrows=max_cnt,
+    )
+    if not os.path.exists(dst_dir):
+        os.makedirs(dst_dir, exist_ok=True)
+    output_file = f"{dst_dir}/output.tsv"
+    if os.path.exists(output_file):
+        uniques = []
+        with open(output_file, "r") as f:
+            for line in f.readlines():
+                videoid = line.strip().split("\t")
+                uniques.append(videoid)
+        data = data[
+            ~data.apply(
+                lambda x: x[move_col] in uniques,
+                axis=1,
+            )
+        ]
+    print(f"Data loaded, total rows to check: {len(data)}")
+    videos = data[move_col].tolist()
+    num_processes = mp.cpu_count()
+    total_rows = len(videos)
+    chunk_size = total_rows // num_processes
+
+
+    lock = mp.Lock()
+
+    processes = []
+    print(f"need to process {total_rows} videos")
+    for i in range(num_processes):
+        p = mp.Process(
+            target=process_check,
+            args=(
+                videos,
+                i * chunk_size,
+                chunk_size,
+                i,
+                dst_dir,
+                num_processes,
+                total_rows,
+                output_file,
+                lock,
+            ),
+        )
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+    return
+
+
 if __name__ == "__main__":
     fire.Fire()
